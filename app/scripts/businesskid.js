d82727e218bf93212881c34e6dfe0e42d0025607
Myles Bieniasz <myles.bieniasz@mlb.com>
2017-04-28 12:19:38 -0400

    resize sky, disable input when paused

--- app/scripts/businesskid.js
+++ app/scripts/businesskid.js
@@ -179,6 +179,7 @@
         console.log("togglePause");
         game.physics.arcade.isPaused = (game.physics.arcade.isPaused) ? false : true;
         var val = isPaused() ? "Paused" : "Play";
+        girl.inputEnabled = !isPaused();
         showLabel(val);
     }
 
